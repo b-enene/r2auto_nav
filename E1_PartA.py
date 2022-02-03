23d10d3149fb58184593ae1598ba67086eb728cc
b-enene <98012202+b-enene@users.noreply.github.com>
2022-02-03 19:35:25 +0800

    Add files via upload
    gong hei fat choy

---
+++ E1_PartA.py
@@ -0,0 +1,26 @@
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD) #GPIO.BCM
+servo_pin = 12
+GPIO.setup(servo_pin, GPIO.OUT)
+p = GPIO.PWM(servo_pin, 50)
+p.start(7.5)
+
+
+def moveservo():
+    while True:
+        angle = input("DegreeAngle:")
+        if angle<0 and angle>180:
+            print("Try again")
+        else:
+            p.ChangeDutyCycle(2.5+5(angle/90))
+
+try:
+    moveservo()
+
+except KeyboardInterrupt:
+    p.stop()
+    GPIO.cleanup()
+
+
+    
